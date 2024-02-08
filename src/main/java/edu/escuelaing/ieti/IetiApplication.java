a248617820e504331cbc6b3705ec04d20949989c
Santiago Gomez <sagomezab@gmail.com>
2024-02-07 19:34:02 -0500

    Comienzo

---
+++ src/main/java/edu/escuelaing/ieti/IetiApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.ieti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IetiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IetiApplication.class, args);
+	}
+
+}
